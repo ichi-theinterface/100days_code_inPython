50a1af2d422ebf6a8ea19ff970fcd1899aff3440
Ichiro Home DT <ichi.theinterface@gmail.com>
2022-04-09 00:19:35 -0700

    de-bugged 04-09-2022

--- day17/quiz-game-start/quiz_brain.py
+++ day17/quiz-game-start/quiz_brain.py
@@ -10,17 +10,14 @@ class QuizBrain:
         self.question_list = input
     
     def still_has_question(self):
-        if self.question_number < len(self.question_list):
-            return True
-        else:
-            False
+        return self.question_number < len(self.question_list)
  
     def next_nexquestion(self):
         # test = self.question_list[self.question_number]
         # print(test)
         
         current_question = self.question_list[self.question_number]
-        self.question_number =+ 1 
+        self.question_number += 1 
         input(f"Q.{self.question_number} : {current_question.text} (True/False)?: ")
 
 
