f69c721d45d593ce0768aa7554d3c3ab611a6fe5
Ichiro Yamasaki <ichi.theinterface@gmail.com>
2022-04-19 09:04:03 -0700

    on 04-19-2022

--- day17/quiz-game-start/quiz_brain.py
+++ day17/quiz-game-start/quiz_brain.py
@@ -1,12 +1,17 @@
 from mimetypes import init
 
-
 class QuizBrain:
 
     def __init__(self, input):
         self.question_number = 0 
         self.question_list = input
     
+    def still_has_questions(self):
+        if self.question_number <= 12:
+            return True
+        else:
+            return False
+
     def next_nexquestion(self):
         # test = self.question_list[self.question_number]
         # print(test)
