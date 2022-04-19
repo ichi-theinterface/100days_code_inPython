817dbbde9835d03561ba6ee8fd07f97a292d2889
Ichiro Yamasaki <ichi.theinterface@gmail.com>
2022-04-19 10:28:41 -0700

    working on day17 on 04-19-2022

--- day17/quiz-game-start/quiz_brain.py
+++ day17/quiz-game-start/quiz_brain.py
@@ -8,25 +8,26 @@ class QuizBrain:
         self.question_number = 0 
         self.question_list = input
     
-<<<<<<< HEAD
-    def still_has_questions(self):
-        if self.question_number <= 12:
-            return True
-        else:
-            return False
-
-=======
     def still_has_question(self):
         return self.question_number < len(self.question_list)
  
->>>>>>> 50a1af2d422ebf6a8ea19ff970fcd1899aff3440
     def next_nexquestion(self):
         # test = self.question_list[self.question_number]
         # print(test)
         
         current_question = self.question_list[self.question_number]
         self.question_number += 1 
-        input(f"Q.{self.question_number} : {current_question.text} (True/False)?: ")
+        user_answer = input(f"Q.{self.question_number} : {current_question.text} (True/False)?: ")
+        self.check_answer(user_answer, current_question.answer)
+
+
+    def check_answer(self, user_answer_input, correct_answer):
+        if user_answer_input == correct_answer:
+            print ("you got it right!!")
+        else:
+            print ("the answer is wrong...")
+
+
 
 
  
